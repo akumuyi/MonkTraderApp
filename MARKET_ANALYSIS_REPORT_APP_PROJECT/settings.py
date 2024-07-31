492406d215572ab9288c37c6fb981dfd36f84419
akumuyi <a.kumuyi@alustudent.com>
2024-07-31 02:47:00 +0200

    migrating files

--- MARKET_ANALYSIS_REPORT_APP_PROJECT/settings.py
+++ MARKET_ANALYSIS_REPORT_APP_PROJECT/settings.py
@@ -30,7 +30,7 @@ DEBUG = True
 
 
 # Define the allowed hosts for the application
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -141,12 +141,12 @@ STATIC_URL = 'static/'
 
 # Ensure STATICFILES_DIRS is set to include static files directory
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'MonkTraderApp', 'static'),
 ]
 
 
 # STATIC_ROOT for production
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
